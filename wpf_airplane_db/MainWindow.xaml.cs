43db1599f0293f357cbab22876f0dd11ad9d17fc
Віталій Лабенський <vitaliy.laben@gmail.com>
2024-05-02 12:40:53 +0300

    Add wpf app

---
+++ wpf_airplane_db/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using EntityFrameworkCore;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace wpf_airplane_db
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        AirplaneDbContext context;
+        public MainWindow()
+        {
+            InitializeComponent();
+            context = new AirplaneDbContext();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            grid.ItemsSource = context.Flights.ToList();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            grid.ItemsSource = context.Airplanes.ToList();
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            grid.ItemsSource = context.Clients.ToList();
+        }
+    }
+}
